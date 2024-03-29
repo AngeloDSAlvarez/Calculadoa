562964acd733a83a89da177435d634a198d45751
AngeloDSAlvarez <angelodaniel1221@gmail.com>
2024-03-28 21:13:37 -0300

    Redução da calculadora, apenas operações com 2 numeros

--- Calculadoa/Program.cs
+++ Calculadoa/Program.cs
@@ -12,104 +12,39 @@ do
         "| 5 - Sair                                                |\n" +
         "-----------------------------------------------------------");
 
-        int opcao = Convert.ToInt16(Console.ReadLine());
+    int opcao = Convert.ToInt16(Console.ReadLine());
+
+    double numeroUm = 0;
+    double numeroDois = 0;
+    if (opcao != 5)
+    {
+
+        Console.WriteLine("Digite o primeio numero: ");
+        numeroUm = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo numero: ");
+        numeroDois = Convert.ToDouble(Console.ReadLine());
+    }
+
 
 
     switch (opcao)
     {
         case 1:
-            Console.WriteLine("Digite os numeros que deseja somar, quando finaliza digite 'sair', tudo minusculo");
-            double soma = 0;
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                if (input == "sair")
-                {
-                    Console.Clear();
-                    Console.WriteLine("O resultado da soma foi: " + soma + "\n");
-                    break;
-                }
-                else
-                {
-                    double valor = Convert.ToDouble(input);
-                    soma += valor;
-                }
-            }
+            Console.WriteLine(numeroUm + " + " + numeroDois + " = " + (numeroUm + numeroDois));
             break;
 
 
         case 2:
-            Console.WriteLine("Digite o numeo que quer subtrair");
-            double numeroTotal = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite os numeros para subtrair, quando finalizar digite 'sair', tudo minusculo");
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                if (input == "sair")
-                {
-                    Console.Clear();
-                    Console.WriteLine("O resultado da soma foi: " + numeroTotal);
-                    break;
-                }
-                else
-                {
-                    double valor = Convert.ToDouble(input);
-                    numeroTotal -= valor;
-                }
-            }
+            Console.WriteLine(numeroUm + " - " + numeroDois + " = " + (numeroUm - numeroDois));
             break;
 
         case 3:
-            Console.WriteLine("Digite os numeros que deseja multiplicar, quando finaliza digite 'sair', tudo minusculo");
-            double mult = Convert.ToDouble(Console.ReadLine());
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                if (input == "sair")
-                {
-                    Console.Clear();
-                    Console.WriteLine("O resultado da multiplicacao foi: " + mult);
-                    break;
-                }
-                else
-                {
-                    double valor = Convert.ToDouble(input);
-                    mult *= valor;
-                }
-            }
+            Console.WriteLine(numeroUm + " x " + numeroDois + " = " + (numeroUm * numeroDois));
             break;
 
         case 4:
-            Console.WriteLine("Digite o numeo que quer dividir");
-            numeroTotal = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite os numeros para dividir, quando finalizar digite 'sair', tudo minusculo");
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                if (input == "sair")
-                {
-                    Console.Clear();
-                    Console.WriteLine("O resultado da divisao foi: " + numeroTotal);
-                    break;
-                }
-                else
-                {
-                    double valor = Convert.ToDouble(input);
-                    if (valor == 0.0)
-                    {
-                        Console.WriteLine("Impossivel dividir por 0, digite outro valor, mas tende ao infinito! ");
-                    }
-                    else
-                    {
-                        numeroTotal /= valor;
-                    }
-
-                }
-            }
+            Console.WriteLine(numeroUm + " / " + numeroDois + " = " + (numeroUm / numeroDois));
             break;
 
         case 5:
